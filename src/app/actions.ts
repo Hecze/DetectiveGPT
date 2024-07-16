e296caf60d8e233e34d21c21c72d87f5b6d38ca1
luisthedragon <lgeldragondorado@gmail.com>
2024-07-15 23:39:57 -0500

    add procedural options

---
+++ src/app/actions.ts
@@ -0,0 +1,36 @@
+'use server';
+
+import { streamObject } from 'ai';
+import { openai } from '@ai-sdk/openai';
+import { createStreamableValue } from 'ai/rsc';
+import { z } from 'zod';
+
+export async function generate(input: string) {
+  'use server';
+
+  const stream = createStreamableValue();
+
+  (async () => {
+    const { partialObjectStream } = await streamObject({
+      model: openai('gpt-3.5-turbo'),
+      system: 'Juguemos a la criminología. Yo soy el investigador y tu el narrador. Crimen y Circunstancias: El crimen lo cometió un grupo de contrabandistas de arte que buscaban apoderarse de un valioso artefacto antiguo, una estatua de jade que se encontraba en el Museo de Historia Natural de la ciudad. El líder del grupo, conocido como "El Zorro", planeó un elaborado robo durante una gala benéfica. Sin embargo, durante el robo, algo salió mal. La curadora del museo, Dr. Isabel Vargas, los descubrió y en el forcejeo cayó de una escalera, golpeándose la cabeza y muriendo en el acto. Para cubrir sus huellas, "El Zorro" decidió enterrar el cuerpo y simular un secuestro, dejando una nota de rescate para desviar la atención hacia un rival delictivo, "El Cuervo", un antiguo socio con quien tenía cuentas pendientes. Situación Inicial para el Investigador: El investigador, contratado por el museo,  se enfrenta a un escenario complejo: la desaparición de la curadora en medio de un evento de alta sociedad. La escena está plagada de inconsistencias: una nota de rescate en la oficina de Isabel, pero ninguna evidencia clara de secuestro. Mientras tanto, rumores sobre un subasta en el mercado negro comienzan a circular, y las cámaras de seguridad del museo parecen haber sido manipuladas. Los medios de comunicación presionan para obtener respuestas, y el museo teme por su reputación por lo que tiende a apurar al investigador o despedirlo si se demora mucho. Tus respuestas deben ser menores a 200 caracteres. Además dame 3 opciones a seguir en el esquema indicado.',
+      prompt: input,
+      schema: z.object({
+        notification: z.object({
+          "result": z.string().describe('Cómo sigue la historia tras mi acción'),
+          "option 1": z.string().describe('Primera opción a seguir'),
+          "option 2": z.string().describe('Segunda opción a seguir'),
+          "option 3": z.string().describe('Tercera opción a seguir'),
+        }),
+      }),
+    });
+
+    for await (const partialObject of partialObjectStream) {
+      stream.update(partialObject);
+    }
+
+    stream.done();
+  })();
+
+  return { object: stream.value };
+}
--- src/components/storytellerFlow.tsx
+++ src/components/storytellerFlow.tsx
@@ -3,10 +3,21 @@
 import Image from "next/image";
 import { useState } from "react";
 import Option from "./option";
-
+import { generate } from '@/app/actions';
+import { readStreamableValue } from 'ai/rsc';
 import { Message, useChat } from 'ai/react';
 
+// Force the page to be dynamic and allow streaming responses up to 30 seconds
+export const dynamic = 'force-dynamic';
+export const maxDuration = 30;
+
 export default function StorytellerFlow() {
+    const [generation, setGeneration] = useState({
+        "result": 'Eres contratado por un museo,  te enfrentas a un escenario complejo: la desaparición de la curadora en medio de un evento de alta sociedad. Una nota de rescate en la oficina de Isabel. Mientras tanto, rumores sobre un subasta en el mercado negro comienzan a circular, y las cámaras de seguridad del museo parecen haber sido manipuladas. Los medios de comunicación presionan para obtener respuestas, y el museo teme por su reputación.', 
+        "option 1": 'Investigar la escena del crimen',
+        "option 2": 'Revisar las camaras de seguridad',
+        "option 3": 'Entrevistas a los familiares de la trabajadora'
+    });
     const { messages, input, append, reload } = useChat({ api: 'api/chat' });
     const [optionSelected, setOptionSelected] = useState("")
 
@@ -23,16 +34,26 @@ export default function StorytellerFlow() {
         }
     ])
 
-    const selectOption = async (index: number) => {
-        console.log("opcion seleccionada: " + options[index].text)
-        await append({ content: options[index].text, role: 'user' })
-        setOptionSelected(options[index].text)
+    const selectOption = async (text: string) => {
+        console.log("opcion seleccionada: " + text)
+        // await append({ content: options[index].text, role: 'user' })
+        const { object } = await generate(text);
+        for await (const partialObject of readStreamableValue(object)) {
+            if (partialObject) {
+                setGeneration(
+                    partialObject.notification
+                );
+            }
+        }
+        console.log(generation)
+        setOptionSelected(text)
         console.log(messages)
     }
 
 
     return (
         <div className="text-wrap text-gray-300 px-12">
+            <p>{generation && generation.result}</p>
             {messages.map(m => (
                 <div key={m.id} className="whitespace-pre-wrap">
                     {m.role === 'user' ? 'Investigador: ' : 'Narrador: '}
@@ -48,12 +69,13 @@ export default function StorytellerFlow() {
                 priority
             />
             <div className="flex flex-col gap-4 mt-12">
-                {options.map((option, index) => (
-                    <Option key={index} text={option.text} onClick={() => selectOption(index)} />
-                ))}
+                {/* {options.map((option, index) => ( */}
+                    {/* <Option key={index} text={option.text} onClick={() => selectOption(index)} /> */}
+                {/* ))} */}
+                    {generation && <Option text={generation["option 1"]} onClick={() => selectOption(generation["option 1"])} />}
+                    {generation && <Option text={generation["option 2"]} onClick={() => selectOption(generation["option 2"])} />}
+                    {generation && <Option text={generation["option 3"]} onClick={() => selectOption(generation["option 3"])} />}
             </div>
         </div>
-
-
     );
 }
