8b75fd843ecb41f32777a724111f4c1dc8ffcf9b
luisthedragon <lgeldragondorado@gmail.com>
2024-07-27 22:03:32 -0500

    delete initialSettings test page

--- src/app/initial-settings-test/page.tsx
+++
@@ -1,6 +0,0 @@
-import InitialSettings from '@/components/initialSettings';
-
-export default function Test() {
-//   return <InitialSettings></InitialSettings>;
-  return <InitialSettings handleStartGame={null}/>    
-}
